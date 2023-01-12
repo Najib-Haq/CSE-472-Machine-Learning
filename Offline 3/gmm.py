5b81d26e39cd883443e8a377ff24712c6d9a6a55
Najibul Haque Sarker <najibhaq98@gmail.com>
2023-01-12 20:44:16 +0600

    offline 3 tsne

--- Offline 3/gmm.py
+++ Offline 3/gmm.py
@@ -5,6 +5,8 @@ import scipy.stats as stats
 import matplotlib.pyplot as plt
 import imageio
 
+from sklearn.manifold import TSNE
+
 class GMM:
     def __init__(self, data, k, max_iter=100, init_pi_random=True):
         self.data = data
@@ -17,21 +19,25 @@ class GMM:
         minx = np.min(self.data)
         maxx = np.max(self.data)
         # Initialize parameters
-        self.mu = np.random.randint(minx, maxx, size=(k, self.dimension)) # k models each with mean vector of dimension
+        # self.mu = np.random.randint(minx, maxx, size=(k, self.dimension)) # k models each with mean vector of dimension
+        # self.mu = np.random.randn(k, self.dimension)
+        cluster_assignments = np.random.randint(0, k, size=self.data.shape[0])
+        self.mu = np.array([self.data[cluster_assignments == i].mean(axis=0) for i in range(k)])
+        self.sigma = np.array([np.cov(self.data[cluster_assignments == i].T) for i in range(k)])
 
         # This needs to be a positive semi-definite matrix
         # https://stackoverflow.com/questions/619335/a-simple-algorithm-for-generating-positive-semidefinite-matrices
-        # self.sigma = np.random.rand(k, self.dimension, self.dimension) # k models each with covariance matrix of dimension
+        # self.sigma = np.random.randn(k, self.dimension, self.dimension) # k models each with covariance matrix of dimension
         # self.sigma = np.ones((k, self.dimension, self.dimension)) # k models each with covariance matrix of dimension
-        self.noise = np.ones((self.dimension, self.dimension))*1e-4 # need to avoid singularity issues
+        # self.noise = np.ones((self.dimension, self.dimension))*1e-6 # need to avoid singularity issues
         # for idx in range(k):
         #     # self.sigma[idx] *= np.array([random.uniform(minx, maxx) for i in range(self.dimension*self.dimension)]).reshape(self.dimension, self.dimension)
-        #     self.sigma[idx] *= np.random.randint(minx, maxx, size=(self.dimension, self.dimension)) + self.noise
+        #     # self.sigma[idx] *= np.random.randint(minx, maxx, size=(self.dimension, self.dimension)) + self.noise
         #     self.sigma[idx] = np.dot(self.sigma[idx], self.sigma[idx].transpose())
 
-        self.sigma = np.zeros((k, self.dimension, self.dimension))
-        for idx in range(k):
-            np.fill_diagonal(self.sigma[idx], 5)
+        # self.sigma = np.zeros((k, self.dimension, self.dimension))
+        # for idx in range(k):
+        #     np.fill_diagonal(self.sigma[idx], 5)
 
         self.pi = np.random.rand(k) if self.init_pi_random else np.ones(k) # k models each with a weight
         self.pi = self.pi / np.sum(self.pi) # normalize weights
@@ -44,7 +50,7 @@ class GMM:
         # for each model
         for i in range(self.responsibilities.shape[1]):
             # calculate the probability of all data point belonging to the model
-            normal_dist = stats.multivariate_normal(self.mu[i], self.sigma[i]+self.noise)
+            normal_dist = stats.multivariate_normal(self.mu[i], self.sigma[i]) #+self.noise)
             self.responsibilities[:, i] = self.pi[i] * normal_dist.pdf(self.data)
         # normalize the probabilities
         # TODO: check if this is correct, also "nvalid value encountered in true_divide can occur -> add tolerance?"
@@ -63,9 +69,11 @@ class GMM:
                 print("NAN")
                 for i in self.responsibilities[:, i]:
                     print(i)
+
+            self.responsibilities += 1e-6 # add pseudocount
             self.mu[i] = np.sum(self.responsibilities[:, i].reshape(self.data.shape[0], 1) * self.data, axis=0) / np.sum(self.responsibilities[:, i])
             # update the covariance matrix, TODO: need to check this
-            self.sigma[i] = (np.dot((self.responsibilities[:, i].reshape(self.data.shape[0], 1) * (self.data - self.mu[i])).transpose(), self.data - self.mu[i]) + self.noise) / np.sum(self.responsibilities[:, i])
+            self.sigma[i] = (np.dot((self.responsibilities[:, i].reshape(self.data.shape[0], 1) * (self.data - self.mu[i])).transpose(), self.data - self.mu[i])) / np.sum(self.responsibilities[:, i]) # self.noise added here too
             # update the weight
             self.pi[i] = np.sum(self.responsibilities[:, i]) / self.data.shape[0]
 
@@ -74,15 +82,14 @@ class GMM:
         # for each model
         for i in range(self.responsibilities.shape[1]):
             # calculate the probability of all data point belonging to the model
-            normal_dist = stats.multivariate_normal(self.mu[i], self.sigma[i])
-
             try:
                 normal_dist = stats.multivariate_normal(self.mu[i], self.sigma[i])
+                # normal_dist = stats.multivariate_normal(self.mu[i], self.sigma[i])
             except:
-                print(self.mu, " ; ", self.sigma)
+                print("ERROR in stats: ", self.mu, " ; ", self.sigma)
             self.responsibilities[:, i] = self.pi[i] * normal_dist.pdf(self.data)
         # normalize the probabilities
-        return np.log(np.sum(self.responsibilities))
+        return np.sum(np.log(np.sum(self.responsibilities, axis=1, keepdims=True)))
 
 
     def fit_k(self, k, plot=False):
@@ -94,6 +101,7 @@ class GMM:
         if plot:
             frames = []
 
+        best_score = 0
         for i in range(self.max_iter):
             self.E_step()
             # print("After E: ", self.sigma, " ; ", self.mu)
@@ -102,8 +110,11 @@ class GMM:
             # print("After M: ", self.sigma, " ; ", self.mu)
             log_likelihood = self.log_likelihood()
             print(f"Sources: {k}, Iteration: {i}, Log-likelihood: {log_likelihood}")
-        
-            if plot and i%3==0: frames.append(self.get_graph(k))
+
+            # plot if log likelihood better 
+            if log_likelihood > best_score:
+                if plot: frames.append(self.get_graph(k))
+                best_score = log_likelihood
 
         if plot: imageio.mimsave(f'gmm_{k}.gif', frames, fps=2)
         return log_likelihood
@@ -112,32 +123,60 @@ class GMM:
         if isinstance(self.k, list):
             log_likelihoods = []
             for k in self.k:
-                log_likelihoods.append((k, self.fit_k(k, plot=k in [2, 3])))
+                log_likelihoods.append((k, self.fit_k(k, plot=False))) #k in [2, 3])))
             return log_likelihoods
         else:
             return self.fit_k(self.k)
 
     def get_graph(self, k):
+        if self.dimension == 2:
+            plot_data = self.data 
+            plot_mu = self.mu
+            plot_sigma = self.sigma
+        else: 
+            tsne = TSNE(n_components=2, learning_rate='auto', init='random', perplexity=3)
+            plot_data = tsne.fit_transform(self.data)
+            # TODO: how to only infer and how to handle sigma?
+            plot_mu = tsne.fit_transform(self.mu)
+            plot_sigma = np.zeros((k, 2, 2))
+            plot_sigma[:, 0] = tsne.fit_transform(self.sigma[:, 0])
+            plot_sigma[:, 1] = tsne.fit_transform(self.sigma[:, 1])
+            # for i in range(self.sigma.shape[0]):
+            #     plot_sigma[i] = tsne.fit_transform(self.sigma[i].reshape(1, -1))
+
         fig, ax = plt.subplots(figsize=(10, 10))
-        ax.scatter(self.data[:, 0], self.data[:, 1], c=self.responsibilities.argmax(axis=1), cmap='viridis')
+        ax.scatter(plot_data[:, 0], plot_data[:, 1], c=self.responsibilities.argmax(axis=1), cmap='viridis')
         for i in range(self.mu.shape[0]):
-            normal_dist = stats.multivariate_normal(self.mu[i], self.sigma[i])
-            ax.contour(np.sort(self.data[:, 0]), np.sort(self.data[:, 1]), normal_dist.pdf(np.dstack(np.meshgrid(np.sort(self.data[:, 0]), np.sort(self.data[:, 1])))), colors='black', alpha=0.3)
+            normal_dist = stats.multivariate_normal(plot_mu[i], plot_sigma[i])
+            ax.contour(np.sort(plot_data[:, 0]), np.sort(plot_data[:, 1]), normal_dist.pdf(np.dstack(np.meshgrid(np.sort(plot_data[:, 0]), np.sort(plot_data[:, 1])))), colors='black', alpha=0.3)
             ax.scatter(self.mu[i, 0], self.mu[i, 1], c='grey', s=100)
-        # plt.savefig(f"graph_{k}.png", bbox_inches='tight')
+        plt.savefig(f"graph_{k}.png", bbox_inches='tight') 
         fig.canvas.draw()
         # Now we can save it to a numpy array.
         data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
         data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
         return data
 
-            
+# def compare_sklearn(data, k=[2, 3, 4, 5, 6, 7, 8, 9, 10]):
+#     from sklearn.mixture import GaussianMixture
+
+#     score = []
+#     for n_clusters in k:
+#         gmm = GaussianMixture(n_components=n_clusters, max_iter=100, init_params='random')
+#         gmm.fit(data)
+#         score.append(gmm.score(data))
+#         print(f"{n_clusters} => {score[-1]}")
+
+#     plt.plot(score)
+#     plt.savefig(f"scores_sklearn.png", bbox_inches='tight') 
 
 if __name__ == "__main__":
     # Load data
-    data = pd.read_csv("data2D.txt", sep=" ", header=None)
+    data = pd.read_csv("data6D.txt", sep=" ", header=None)
     data = data.values
 
+    # compare_sklearn(data, k=[2, 3, 4, 5, 6, 7, 8, 9, 10])
     gmm = GMM(data, k=[2, 3, 4, 5, 6, 7, 8, 9, 10], max_iter=100, init_pi_random=True)
     print(gmm.fit())
+
 
